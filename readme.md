57f0384cfa7df1959bc96963388dc752cd55ebfd
devjanaprime <dev@primeacademy.io>
2016-08-02 09:44:56 -0500

    deck array and card objects

---
+++ readme.md
@@ -0,0 +1,13 @@
+Vanilla JS Deck Shuffler
+========================
+
+Just an exercise to keep some of my vanilla JavaScript and lecturing skills in order before a new cohort starts.
+
+Functions, Objects, Arrays.
+
+Features by branch:
+0.1 - deck array and card objects
+0.2 - basic deck on DOM
+0.3 - cut deck
+0.4 - shuffle deck
+0.5 - random card
---
+++ scripts/deckShuffler.js
@@ -0,0 +1,26 @@
+console.log( 'deckShuffler.js sourced' );
+
+// this array will hold our deck of cards
+var deck = [];
+// the possible suits
+var suits = [ 'Clubs', 'Diamonds', 'Hearts', 'Spades' ];
+// the possible faces
+var faces = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King' ];
+
+var setupDeck = function(){
+  // use suits and faces to create the deck
+  for( var i=0; i < suits.length; i++){
+    for( var j=0; j < faces.length; j++ ){
+      // create a new card with the current face/suit combination
+      var newCard={
+        suit: suits[ i ],
+        face: faces [ j ]
+      };
+      //push the new card into the deck
+      deck.push( newCard );
+    } // end faces loop
+  } // end suits loop
+  console.log( deck );
+}
+//run the setupDeck Function
+setupDeck();

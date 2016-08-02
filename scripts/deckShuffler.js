8a617839f94e8aae639c4a7744826e4642acb52e
devjanaprime <dev@primeacademy.io>
2016-08-02 11:42:03 -0500

    added shuffle

--- scripts/deckShuffler.js
+++ scripts/deckShuffler.js
@@ -67,8 +67,30 @@ var showDeck = function(){
   document.getElementById( 'outputDiv' ).appendChild( outputList );
 } // end funk
 
+// shuffle array function from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 var shuffleDeck = function(){
   console.log( 'in shuffleDeck' );
+  deck = shuffle( deck );
+  showDeck();
 } // end funk
 
 //run the setupDeck Function
